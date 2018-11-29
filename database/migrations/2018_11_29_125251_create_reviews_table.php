20c75d79b04b1f4b8098e806927c300644a54785
krishnashrestha99 <lifeisgood.ks99@gmail.com>
2018-11-29 19:39:01 +0545

    Migration created for product and reviews

--- database/migrations/2018_11_29_125251_create_reviews_table.php
+++ database/migrations/2018_11_29_125251_create_reviews_table.php
@@ -15,6 +15,11 @@ class CreateReviewsTable extends Migration
     {
         Schema::create('reviews', function (Blueprint $table) {
             $table->increments('id');
+            $table->unsignedInteger('product_id');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+            $table->string('customer');
+            $table->text('review');
+            $table->double('star');
             $table->timestamps();
         });
     }
